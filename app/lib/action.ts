e66cdb56000807972150255ef6154f04587559f5
meligonzalezn <melissa.gonzalez.nebrijo@gmail.com>
2025-05-11 16:34:34 -0500

    fix build

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -60,7 +60,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     return {
-      message: 'Database Error: Failed to Create Invoice.',
+      message: `Database Error: Failed to Create Invoice. ${error}`,
     };
   }
  
@@ -96,7 +96,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    return { message: `Database Error: Failed to Update Invoice ${error}`};
   }
  
   revalidatePath('/dashboard/invoices');
@@ -108,7 +108,7 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    return { message: `Database Error: Failed to Delete Invoice ${error}`};
   }
   revalidatePath('/dashboard/invoices');
 }
